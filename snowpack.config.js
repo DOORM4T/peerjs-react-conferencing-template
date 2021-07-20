31ea3e778def24855859b519af7c34e64aef474b
Mat Seto <seanseto@gmail.com>
2021-07-19 22:01:39 -0700

    Add babel

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,12 +4,24 @@ module.exports = {
     src: "/dist",
   },
   plugins: [
-    "@snowpack/plugin-typescript", 
+    "@snowpack/plugin-typescript",
     "@snowpack/plugin-react-refresh",
-    '@snowpack/plugin-webpack'
+    "@snowpack/plugin-webpack",
+    [
+      "@snowpack/plugin-babel",
+      {
+        transformOptions: {
+          presets: [
+            "@babel/preset-typescript",
+            "@babel/preset-react",
+            "@babel/preset-env",
+          ],
+        },
+      },
+    ],
   ],
   buildOptions: {
     baseUrl: "/peerjs-react-conferencing-template",
-    metaUrlPath: "snowpack"
+    metaUrlPath: "snowpack",
   },
 }
